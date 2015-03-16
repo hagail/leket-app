d1479d3d8ef81ecc3c096ce3d078e0a17290c102
Eyal Luxenburg <eyal.luxenburg@klarna.com>
2015-03-16 19:41:54 +0200

    create suppliers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150316173408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string   "priority_id"
+    t.string   "name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "supplier_id"
+  end
+
+  add_index "suppliers", ["supplier_id"], name: "index_suppliers_on_supplier_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "priority_id"
+    t.string   "email"
+    t.string   "name"
+    t.string   "phone"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
