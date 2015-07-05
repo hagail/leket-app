c18057744f29b26e157ac19a8e4fd34f3e80e12e
Eyal Luxenburg <eyal.luxenburg@klarna.com>
2015-07-05 21:15:18 +0300

    fix bug

--- app/controllers/pickup_reports_controller.rb
+++ app/controllers/pickup_reports_controller.rb
@@ -1,6 +1,6 @@
 class PickupReportsController < ApplicationController
   def new
-    @current_pickup = Pickup.find(params[:pickup_id])
-    @pickup_report = current_pickup.pickup_report || ReportBuilder.build_pickup_report(current_pickup)
+    @pickup = Pickup.find(params[:pickup_id])
+    @pickup_report = @pickup.pickup_report || ReportBuilder.build_pickup_report(@pickup)
   end
 end
