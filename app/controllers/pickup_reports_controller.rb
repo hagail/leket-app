679d2a3843e512334e1a965949a147c8a82a1287
Eyal Luxenburg <eyal.luxenburg@klarna.com>
2015-07-05 20:00:39 +0300

    add report builder

--- app/controllers/pickup_reports_controller.rb
+++ app/controllers/pickup_reports_controller.rb
@@ -1,2 +1,6 @@
 class PickupReportsController < ApplicationController
+  def new
+    @current_pickup = Pickup.find(params[:pickup_id])
+    @pickup_report = current_pickup.pickup_report || ReportBuilder.build_pickup_report(current_pickup)
+  end
 end
