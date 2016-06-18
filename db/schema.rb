16aac65ab01275a58ac53a0285e7751ddbbb4c32
Guy Israeli <guy.israeli@klarna.com>
2016-06-18 15:39:55 +0300

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618114816) do
+ActiveRecord::Schema.define(version: 20160618123402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,8 +61,9 @@ ActiveRecord::Schema.define(version: 20160618114816) do
   create_table "pickup_reasons", force: :cascade do |t|
     t.string   "priority_id"
     t.string   "name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.boolean  "user_displayed", default: false
   end
 
   create_table "pickup_reports", force: :cascade do |t|
