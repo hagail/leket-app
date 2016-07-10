b5a9cfdcbe1ebde2d7c8d4ebd115c94b4ccd561d
Guy Israeli <guy.israeli@klarna.com>
2016-07-10 10:18:12 +0300

    add specs to pickup spec

--- spec/models/pickup_spec.rb
+++ spec/models/pickup_spec.rb
@@ -26,7 +26,7 @@ describe Pickup do
                   sent_at:     sent_at)
   end
 
-  describe '.approved?' do
+  describe '#approved?' do
     let(:approved_at) { Time.now }
 
     it 'returns true if approved is filled' do
@@ -34,7 +34,7 @@ describe Pickup do
     end
   end
 
-  describe '.approve!' do
+  describe '#approve!' do
     it 'marks pickups as approved' do
       pickup.approve!
 
@@ -42,11 +42,30 @@ describe Pickup do
     end
   end
 
-  describe '.sent?' do
+  describe "#unapprove!" do
+    let(:approved_at) { Time.now }
+    it "sets approved_at to nil" do
+      pickup.unapprove!
+
+      expect(pickup.approved_at).to be nil
+    end
+  end
+
+  describe '#sent?' do
     let(:sent_at) { Time.now }
 
     it 'returns true if pickup was sent' do
       expect(pickup.sent?).to be_truthy
+    end
+  end
+
+  describe "#sent!" do
+    it "sets the sent_at to the current time" do
+      Timecop.freeze
+
+      pickup.sent!
+
+      expect(pickup.sent_at).to eq Time.now
     end
   end
 end
