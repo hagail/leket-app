41d1e41ac2066e4210b7d1ffa6a2b91e0a621ba0
Guy Israeli <guy.israeli@klarna.com>
2016-07-10 10:17:40 +0300

    add warehouse spec

---
+++ spec/models/warehouse_spec.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: warehouses
+#
+#  id          :integer          not null, primary key
+#  priority_id :string
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  city        :string
+#  address     :string
+#
+
+
+require 'spec_helper'
+
+describe Warehouse do
+  describe "#name_with_address" do
+    it "returns the name with address" do
+      warehouse = Warehouse.create(name: "test", address: "some street")
+
+      expect(warehouse.name_with_address).to eq "test - some street"
+    end
+  end
+end
