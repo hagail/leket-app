d9ee4efdaba27d181d3acdd3419c7553bd65baf4
Guy Israeli <guy.israeli@klarna.com>
2016-08-21 10:49:14 +0300

    add supplier spec fix

--- spec/models/supplier_report_spec.rb
+++ spec/models/supplier_report_spec.rb
@@ -80,6 +80,22 @@ describe SupplierReport do
     end
   end
 
+  describe "#top_supplier?" do
+    context "supplier has no supplier" do
+      let(:supplier) { create(:supplier, supplier_id: nil) }
+      it "return the supplier" do
+        expect(supplier.top_supplier?).to eq true
+      end
+    end
+
+    context "supplier has a parent supplier" do
+      let(:supplier) { create(:supplier, supplier_id: 5) }
+      it "returns the parent supplier" do
+        expect(supplier.top_supplier?).to eq false
+      end
+    end
+  end
+
   describe "#pickup_reason_by_condition!" do
     context "there was a collection of food with no reason" do
       let!(:reason_food) { PickupReason.create(priority_id: "01") }
